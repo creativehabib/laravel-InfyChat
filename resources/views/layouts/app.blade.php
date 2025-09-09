563f62f8de3c73e4e485078cd3b0fba6e8fcd89d
Habibur Rahaman <shamolrahaman@gmail.com>
2025-09-09 08:08:14 +0600

    Fix button loading state without Bootstrap jQuery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -188,16 +188,6 @@
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
         },
     });
-    (function ($) {
-        $.fn.button = function (action) {
-            if (action === 'loading' && this.data('loading-text')) {
-                this.data('original-text', this.html()).html(this.data('loading-text')).prop('disabled', true)
-            }
-            if (action === 'reset' && this.data('original-text')) {
-                this.html(this.data('original-text')).prop('disabled', false);
-            }
-        };
-    }(jQuery));
     $(document).ready(function () {
         $('.alert').delay(4000).slideUp(300);
     });
